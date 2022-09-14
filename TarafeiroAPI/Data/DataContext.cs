020d063a13fdaa5617fa6bc65a9b70bc70ccc105
Marquescharlon Santos <marquescharlonsantos@cartaodetodos.com>
2022-09-14 13:50:56 -0300

    Adicionando dependências e criando DataContext.cs

---
+++ TarafeiroAPI/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TarafeiroAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options) { }
+
+        public DbSet<Tarefa> Tarefas { get; set; }  
+        public DbSet<TarefaTipo> TarefasTipos { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+    }
+}
