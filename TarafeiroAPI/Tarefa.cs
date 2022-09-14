00a15906a9d64565aed138ad7885f525f1dc328b
Marquescharlon Santos <marquescharlonsantos@cartaodetodos.com>
2022-09-14 13:36:10 -0300

    Criando a classe Tarefa.cs

---
+++ TarafeiroAPI/Tarefa.cs
@@ -0,0 +1,11 @@
+﻿namespace TarafeiroAPI
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public string Comentario { get; set; } = string.Empty;
+        public int TarefaTipoId { get; set; }
+        public TarefaTipo? TarefaTipo { get; set; }
+    }
+}
